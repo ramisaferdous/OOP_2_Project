a3aedc52b66f59dee4d95fb54ea83fda7e4f6dfb
ramisaferdous <rifahramisa@iut-dhaka.edu>
2024-01-05 22:46:04 +0600

    Added Customer and User class

---
+++ Customer.java
@@ -0,0 +1,86 @@
+import java.util.*;
+public class Customer {
+    private  String userID;
+    private String email;
+    private String name;
+    private String phone;
+    private final String password;
+    private String address;
+    private int age;
+    //    public List<Flight> flightsRegisteredByUser;
+    public List<Integer> numOfTicketsBookedByUser;
+//    public static final List<Customer> customerCollection = User.getCustomersCollection();
+
+    Customer() {
+        this.userID = null;
+        this.name = null;
+        this.email = null;
+        this.password = null;
+        this.phone = null;
+        this.address = null;
+        this.age = 0;
+    }
+    Customer(String name, String email, String password, String phone, String address, int age) {
+
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phone = phone;
+        this.address = address;
+        this.age = age;
+        this.numOfTicketsBookedByUser = new ArrayList<>();
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Integer> getNumOfTicketsBookedByUser() {
+        return numOfTicketsBookedByUser;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
